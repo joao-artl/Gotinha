ba4aabffc56db5b791d5d960f0d93b27c5b8cf8d
Eric Silveira Gomes <es9694649@gmail.com>
2023-10-24 22:43:33 -0300

    Header

--- front-end/src/componentes/Home.js
+++ front-end/src/componentes/Home.js
@@ -4,15 +4,8 @@ import Header from './Cabecalho.js';
 
 function Home() {
     return (
-        <div className="header">
-            <div className="logo">
-                <img src={LogoGO} alt="Minha Imagem" className="logoGO" />
-            </div>
-            <div className="navegacaoMenu">
-                <a>Home</a>
-                <a>Pesquisar</a>
-                <a>Sobre</a>
-            </div>
+        <div>
+            <Header />
         </div>
     );
 }
