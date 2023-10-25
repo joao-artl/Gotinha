1a0d9c51c18a83997f102d965e6ffa67cd9c652e
Eric Silveira Gomes <es9694649@gmail.com>
2023-10-25 01:05:21 -0300

    Finalizado

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Cabecalho from './componentes/Cabecalho.js';
+import Home from './componentes/Home.js';
 
 function App() {
   return (
     <div className="App">
       <Cabecalho />
+      <Home />
     </div>
   );
 }
